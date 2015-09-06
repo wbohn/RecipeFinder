81c4044cb3c5bb0bd45742fff38584ee82098540
wbohn <bohn.wd@gmail.com>
2015-09-05 21:48:58 -0500

    add load animation, network error messages and refresh button

--- app/src/main/java/com/wbohn/recipefinder/MainActivity.java
+++ app/src/main/java/com/wbohn/recipefinder/MainActivity.java
@@ -6,11 +6,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.squareup.otto.Subscribe;
+import com.wbohn.recipefinder.Bus.ErrorEvent;
+import com.wbohn.recipefinder.Bus.RecipeRequest;
+import com.wbohn.recipefinder.Bus.RecipesReceivedEvent;
+import com.wbohn.recipefinder.Bus.RefreshRequest;
 import com.wbohn.recipefinder.Network.PuppyClient;
 
 public class MainActivity extends AppCompatActivity {
     private PuppyClient puppyClient;
+
+    private ProgressBar loadingIcon;
+    private TextView emptyText;
+    private TextView recipePuppyLink;
+
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
             puppyClient = new PuppyClient();
             fragmentManager.beginTransaction().add(puppyClient, "puppyClient").commit();
         }
+
+        loadingIcon = (ProgressBar) findViewById(R.id.loadingIcon);
+        emptyText = (TextView) findViewById(R.id.empty_text);
+        recipePuppyLink = (TextView) findViewById(R.id.link);
     }
 
     @Override
@@ -46,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        this.menu = menu;
         return true;
     }
 
@@ -60,5 +79,44 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Subscribe
+    public void onRecipeRequest(RecipeRequest request) {
+        hideError();
+        loadingIcon.setVisibility(View.VISIBLE);
+    }
+
+    @Subscribe
+    public void onRecipesReceived(RecipesReceivedEvent event) {
+        hideError();
+
+        loadingIcon.setVisibility(View.GONE);
+        if (menu != null) {
+            menu.findItem(R.id.action_refresh).setVisible(false);
+        }
+    }
+
+    @Subscribe
+    public void onError(ErrorEvent error) {
+        loadingIcon.setVisibility(View.GONE);
+        emptyText.setVisibility(View.VISIBLE);
+        emptyText.setText(getResources().getString(R.string.network_error));
+        recipePuppyLink.setVisibility(View.VISIBLE);
+
+        if (menu != null) {
+            menu.findItem(R.id.action_refresh).setVisible(true);
+        }
+    }
+
+    @Subscribe
+    public void onRefreshRequest(RefreshRequest request) {
+        hideError();
+        loadingIcon.setVisibility(View.VISIBLE);
+    }
+
+    private void hideError() {
+        emptyText.setVisibility(View.GONE);
+        recipePuppyLink.setVisibility(View.GONE);
     }
 }
